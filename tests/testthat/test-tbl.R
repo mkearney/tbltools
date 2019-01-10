60ed5931e036798eb718ae2e9f72334c00f934be
Michael W. Kearney <kearneymw@missouri.edu>
2019-01-10 16:05:20 -0600

    add tests

--- tests/testthat/test-tbl.R
+++ tests/testthat/test-tbl.R
@@ -1,9 +1,39 @@
 context("test-tbl")
 
 test_that("as_tbl, tabsort, ntbl", {
+  env <- environment()
+  e <- data.frame(
+    a = letters,
+    b = LETTERS,
+    c = seq_along(letters),
+    d = data.frame(aa = letters, bb = LETTERS, cc = seq_along(letters)),
+    row.names = rev(letters)
+  )
   d <- data.frame(abc = sample(letters[1:3], 100, replace = TRUE),
     xyz = sample(letters[24:26], 100, replace = TRUE),
     stringsAsFactors = FALSE)
+  env_tbls(env)
+  expect_true(inherits(d, "tbl_df"))
+  expect_true(inherits(e, "tbl_df"))
+  e <- data.frame(
+    a = letters,
+    b = LETTERS,
+    c = seq_along(letters),
+    d = data.frame(aa = letters, bb = LETTERS, cc = seq_along(letters)),
+    row.names = rev(letters)
+  )
+  d <- data.frame(abc = sample(letters[1:3], 100, replace = TRUE),
+    xyz = sample(letters[24:26], 100, replace = TRUE),
+    stringsAsFactors = FALSE)
+  e <- as_tbl(e, row_names = TRUE)
+  expect_named(e)
+  expect_true("d.bb" %in% names(e))
+  expect_equal(ncol(e), 7)
+  expect_equal(nrow(e), 26)
+  expect_true(is.factor(e$d.aa))
+  expect_true(is.data.frame(e))
+  expect_true(is.character(e$row_names))
+
   expect_true(inherits(as_tbl(d), "tbl_df"))
   d <- tabsort(d)
   expect_equal(ncol(d), 4)
@@ -11,6 +41,11 @@ test_that("as_tbl, tabsort, ntbl", {
   expect_true(inherits(as_tbl(d), "tbl_df"))
   expect_true(is.data.frame((ntbl(d, n))))
   expect_equal(ncol(ntbl(d, n)), 2)
+  #e$.data <- sample(c("a", "b", "c"), nrow(e), replace = TRUE)
+  expect_warning(
+    tabsort(.data = sample(c("a", "b", "c"), nrow(e), replace = TRUE),
+      n = sample(c("a", "b", "c"), nrow(e), replace = TRUE))
+  )
 })
 
 test_that("filter_rows, arrange_rows", {
