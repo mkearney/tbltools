377e9f9a745c80f6843b9d099404f743e948713f
Michael W. Kearney <kearneymw@missouri.edu>
2019-01-11 14:16:16 -0600

    rm print

--- R/print.R
+++ R/print.R
@@ -1,19 +1,19 @@
-
-#' @export
-print.tbl_df <- function(x) {
-  if (!isNamespaceLoaded("tibble")) {
-    pcat(gray_text("# A pseudo tibble: ", nrow(x), " x ", ncol(x)))
-    #m <- ncol(x) * 10
-    x <- as.data.frame(x)
-    x <- head(x, 10)
-    is_chr <- sapply(x, is.character)
-    x[is_chr] <- lapply(x[is_chr], substr, 1, 15)
-    chars <- sapply(seq_len(ncol(x)), function(i)
-      1 + max(nchar(c(names(x)[i], x[[i]])), na.rm = TRUE))
-    w <- getOption("width", 80)
-    kp <- which(cumsum(chars) < w)
-    print(x[kp])
-  } else {
-    tibble:::print.tbl(x)
-  }
-}
+#'
+#' #' @export
+#' print.tbl_df <- function(x) {
+#'   if (!isNamespaceLoaded("tibble")) {
+#'     pcat(gray_text("# A pseudo tibble: ", nrow(x), " x ", ncol(x)))
+#'     #m <- ncol(x) * 10
+#'     x <- as.data.frame(x)
+#'     x <- head(x, 10)
+#'     is_chr <- sapply(x, is.character)
+#'     x[is_chr] <- lapply(x[is_chr], substr, 1, 15)
+#'     chars <- sapply(seq_len(ncol(x)), function(i)
+#'       1 + max(nchar(c(names(x)[i], x[[i]])), na.rm = TRUE))
+#'     w <- getOption("width", 80)
+#'     kp <- which(cumsum(chars) < w)
+#'     print(x[kp])
+#'   } else {
+#'     tibble:::print.tbl(x)
+#'   }
+#' }
