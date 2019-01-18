75dffba6e41b1daefd73eae5dd1d760dbdf27a9f
Michael W. Kearney <kearneymw@missouri.edu>
2019-01-18 05:33:33 -0600

    update

--- R/n_obs.R
+++ R/n_obs.R
@@ -7,7 +7,7 @@
 #' @export
 n_obs <- function() {
   if (identical(parent.frame(), base::.GlobalEnv)) {
-    stop("n_obs() should not be used in global environment", call. = FALSE)
+    stop("`n_obs()` should not be used in global environment", call. = FALSE)
   }
   vars <- ls(all.names = TRUE, envir = parent.frame())
   if (length(vars) == 0) return(0)
--- R/pull.R
+++ R/pull.R
@@ -1,9 +1,22 @@
-
-
+#' Pull data
+#'
+#' Pull (extract) data from a data frame
+#'
+#' @param .data Input data frame
+#' @param ... Unquoted name of column to pull from data frame
+#' @return A column pulled from its data frame (inheriting whatever class the
+#'   column is)
+#' @examples
+#'
+#' ## pull the 'y' variable from the data frame
+#' tbl_data(x = rnorm(5), y = letters[1:5]) %>% pull_data(y)
+#'
+#' @export
 pull_data <- function(.data, ...) {
   UseMethod("pull_data")
 }
 
+#' @export
 pull_data.default <- function(.data, ...) {
-  eval(capture_dots(...)[[1]], .data, parent.frame())
+  eval(capture_dots(...)[[1]], .data, new.env(parent = emptyenv()))
 }
